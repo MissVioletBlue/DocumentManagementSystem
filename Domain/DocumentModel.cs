69c53a637d57da0e01d8e9985431418358636712
rokudara-sen <matteo.habsburg@gmx.at>
2025-09-15 16:09:59 +0200

    UNFINISHED!

---
+++ Domain/DocumentModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain;
+
+public class DocumentModel
+{
+    [Required]
+    public int UniqueIdentifier { get; set; }
+    public string? DocumentTitle { get; set; }
+    public string? DocumentLocation { get; set; }
+    
+    // Metadata
+    
+    public TimeSpan? DocumentCreationDate { get; set; }
+    public string? DocumentAuthor { get; set; }
+    public List<string>? DocumentTags { get; set; }
+    
+    
+}
---
+++ Infrastructure/DocumentDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure;
+
+public class DocumentDbContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+    
+    public DbSet<DocumentModel> Documents { get; set; }
+    
+    public DocumentDbContext(IConfiguration configuration){
+        _configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(_configuration.GetConnectionString("DefaultConnection"));
+    }
+    
+    
+}
