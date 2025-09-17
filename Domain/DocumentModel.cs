5c490ff87523e09ef8e3fea185dd18c5ec80ab4a
rokudara-sen <160833839+rokudara-sen@users.noreply.github.com>
2025-09-17 19:28:29 +0200

    Sprint 1 commited
    * Added health-check endpoint
    
    * Postgres and PGAdmin in docker compose file
    
    * UNFINISHED!
    
    * Connection String and EF core identifiers
    
    * Migration and Dockerfile
    
    * Fixes
    
    * Migration fully working
    
    * Sprint 1 finished

---
+++ Domain/DocumentModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain;
+
+public class DocumentModel
+{
+    [Key]
+    public int UniqueIdentifier { get; set; }
+    [Required, MaxLength(256)]
+    public string? DocumentTitle { get; set; }
+    [MaxLength(2048)]
+    public string? DocumentLocation { get; set; }
+    
+    // Metadata
+    
+    public TimeSpan? DocumentCreationDate { get; set; }
+    [MaxLength(256)]
+    public string? DocumentAuthor { get; set; }
+    public string[]? DocumentTags { get; set; }
+    
+    
+}
